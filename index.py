10157c45c3e66b3a1bd8d4a10d87e2f6ae3fa8b1
jeeve <jyvaisj@gmail.com>
2020-12-04 15:23:35 +0100

    init

---
+++ index.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  4 15:14:12 2020
+
+@author: JV
+"""
+
+import cgi 
+
+form = cgi.FieldStorage()
+print("Content-type: text/html; charset=utf-8\n")
+
+print(form.getvalue("name"))
+
+html = """<!DOCTYPE html>
+<head>
+    <title>Mon programme</title>
+</head>
+<body>
+    <form action="/index.py" method="post">
+        <input type="text" name="name" value="Votre nom" />
+        <input type="submit" name="send" value="Envoyer information au serveur">
+    </form> 
+</body>
+</html>
+"""
+
+print(html)
---
+++ server.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  4 15:13:24 2020
+
+@author: JV
+"""
+
+import http.server
+ 
+PORT = 8888
+server_address = ("", PORT)
+
+server = http.server.HTTPServer
+handler = http.server.CGIHTTPRequestHandler
+handler.cgi_directories = ["/"]
+print("Serveur actif sur le port :", PORT)
+
+httpd = server(server_address, handler)
+httpd.serve_forever()
